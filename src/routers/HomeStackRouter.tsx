1735bb44fd51e9fb8ae62a8870fa1d4eab14d5a8
saurabh <saurabh.dixit@triveniconsulting.com>
2020-02-17 18:41:52 +0530

    Changes for stack navigator.

--- src/routers/HomeStackRouter.tsx
+++ src/routers/HomeStackRouter.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Text, Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../screens/Home';
-import HomeCopy from '../screens/Home copy'
+import HomeCopy from '../screens/Home copy';
+import image1 from '../images/image1.png';
 
 const Stack = createStackNavigator();
 
-export default function HomeStackRouter() {
+export default function HomeStackRouter(props: any) {
+    console.log('Props ', props)
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator
+            screenOptions={{
+                headerTintColor: 'white',
+                headerStyle: { backgroundColor: 'tomato' },
+                headerTransparent: true,
+                headerBackground: () => {
+                    return (
+                        <Image source={image1} style={{ height: 60, width: '100%' }} />
+                    )
+                }
+            }}
+        >
+            <Stack.Screen name="Home"
+                component={HomeScreen}
+                options={{
+                    title: 'Home'
+                }} />
             <Stack.Screen name="HomeCopy" component={HomeCopy} />
         </Stack.Navigator>
     );
